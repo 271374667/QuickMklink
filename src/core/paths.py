02c90547bed9200ac07174578789b1dc3926feab
bemake-pycharm <271374667@qq.com>
2024-08-26 22:32:35 +0800

    Details: 完善了系统的文件系统

--- src/core/paths.py
+++ src/core/paths.py
@@ -5,10 +5,17 @@ ROOT = Path(__file__).parent.parent.parent
 
 # Path to the src directory
 LOCALE_DIR = ROOT / 'locale'
+BIN_DIR = ROOT / 'bin'
+ASSETS_DIR = ROOT / 'assets'
+OUTPUT_DIR = ROOT / 'output'
 
 # Path to the file
+LOG_FILE = ROOT / 'QuickMklink.log'
 CONFIG_FILE = ROOT / 'config.json'
 ENTRY_PY_FILE = ROOT / 'QuickMklink.py'
+COPY_BAT_FILE = BIN_DIR / 'copy.bat'
+MOVE_BAT_FILE = BIN_DIR / 'move.bat'
+LOGO_FILE = ASSETS_DIR / 'images' / 'logo.ico'
 
 if __name__ == '__main__':
-    print(ENTRY_PY_FILE)
+    print(MOVE_BAT_FILE)
