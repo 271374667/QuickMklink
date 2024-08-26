0eedef38732ae2be9d513c09125bdc0f73f357d0
bemake-pycharm <271374667@qq.com>
2024-08-26 22:34:35 +0800

    Feature: 新增一个软件级别的单例模式，确保电脑中只有一个实例在运行

--- src/utils/singleton.py
+++ src/utils/singleton.py
@@ -1,3 +1,7 @@
+import time
+from pathlib import Path
+
+
 def singleton(cls):
     instances = {}
 
@@ -7,3 +11,21 @@ def singleton(cls):
         return instances[cls]
 
     return get_instance
+
+
+class SoftwareSingleton:
+    def __init__(self, lock_file: Path):
+        self.lock_file = lock_file
+        self._create_lock_file()
+
+    def _create_lock_file(self):
+        if self.lock_file.exists():
+            print("Another instance is already running. Exiting.")
+            while self.lock_file.exists():
+                time.sleep(0.2)
+        else:
+            self.lock_file.touch()
+
+    def __del__(self):
+        if self.lock_file.exists():
+            self.lock_file.unlink()
