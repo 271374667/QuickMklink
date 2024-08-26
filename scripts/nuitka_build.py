6f79212c077d5152181e8e4d88f341b944448683
bemake-pycharm <271374667@qq.com>
2024-08-26 22:35:12 +0800

    Feature: 新增一个 nuitka 打包脚本(在scripts目录下)

---
+++ scripts/nuitka_build.py
@@ -0,0 +1,17 @@
+import shutil
+import subprocess
+
+from src.core.paths import LOGO_FILE, OUTPUT_DIR, BIN_DIR, ASSETS_DIR, LOCALE_DIR, ENTRY_PY_FILE
+
+print('start')
+command: str = f'nuitka "{ENTRY_PY_FILE}" --standalone --enable-plugin=tk-inter --output-dir="{OUTPUT_DIR}" --windows-icon-from-ico="{LOGO_FILE}"'
+print("command:", command)
+
+subprocess.run(command, shell=True)
+
+# 将bin, assets, locale目录复制到输出目录
+shutil.copytree(BIN_DIR, OUTPUT_DIR/ 'QuickMklink.dist' / 'bin', dirs_exist_ok=True)
+shutil.copytree(ASSETS_DIR, OUTPUT_DIR/ 'QuickMklink.dist' / 'assets', dirs_exist_ok=True)
+shutil.copytree(LOCALE_DIR, OUTPUT_DIR/ 'QuickMklink.dist' / 'locale', dirs_exist_ok=True)
+
+print('finished')
