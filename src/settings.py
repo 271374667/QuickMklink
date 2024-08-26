67746a224c1bd9ab30bd4e32a04cbbe9ad04803d
bemake-pycharm <271374667@qq.com>
2024-08-26 22:39:24 +0800

    Optimization: 现在的设置有了更多的内容

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,17 @@
 from typing import Final
 
+from src.i18n import I18n
+
+_ = I18n().trans
+
 TAG: Final[str] = "QuickMklink"
+TAGCOPY: Final[str] = "QuickMklinkCopy"
+TAGMOVE: Final[str] = "QuickMklinkMove"
 VERSION: Final[str] = "0.8.0"
+WATING_INTERVAL: Final[int] = 100
+ABOUT_TEXT: Final[str] = _("""
+QuickMklink is a tool that helps you create symbolic links quickly.
+The software is open source software, please do not carry out secondary trafficking or commercial purposes
+
+author: PythonImporter
+""")
