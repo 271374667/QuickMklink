0f8f972f51dd2762b4ec1503f0c64aba1dc83584
bemake-pycharm <271374667@qq.com>
2024-08-26 22:39:08 +0800

    Feature: 现在的i18n.py能够提供已经安装的所有语言的列表 Modify: 现在的 i18n 默认文件名称改为 messages

--- src/i18n.py
+++ src/i18n.py
@@ -9,7 +9,7 @@ from src.utils.singleton import singleton
 
 
 class Languages:
-    _domain: str = 'default'
+    _domain: str = 'messages'
     _languages: str = 'en_US'
     _localedir: Path = paths.LOCALE_DIR
     _instance: "Languages" = None
@@ -55,6 +55,10 @@ class Languages:
         """Check if the system language is the same as the language of the application."""
         raise NotImplementedError('You must implement the is_system_language property.')
 
+    @classmethod
+    def get_installed_languages_list(cls) -> list[Type['Languages']]:
+        return cls._installed_languages_list
+
     def __repr__(self):
         return f'domain: {self.domain}, localedir: {self.localedir}, languages: {self.languages}'
 
@@ -70,11 +74,20 @@ class LanguagesChinese(Languages):
         return 'Chinese' in locale_setting
 
 
+class LanguagesEnglish(Languages):
+    _languages: str = 'en_US'
+
+    @property
+    def is_system_language(self) -> bool:
+        return False
+
+
 @singleton
 class I18n:
     def __init__(self):
         config_language_name: str = cfg.get(cfg.language)
         self._config_language: Languages = Languages.get_language_by_name(config_language_name)
+        self._languages: list[Languages] = [x() for x in Languages.get_installed_languages_list()]
 
     @property
     def trans(self) -> Callable:
@@ -85,6 +98,10 @@ class I18n:
         )
         lang.install()
         return lang.gettext
+
+    @property
+    def languages(self) -> list[Languages]:
+        return self._languages
 
     def __repr__(self):
         return f'config_language: {self._config_language.domain}, config_localdir:{self._config_language.localedir}, config_language:{self._config_language.languages}'
